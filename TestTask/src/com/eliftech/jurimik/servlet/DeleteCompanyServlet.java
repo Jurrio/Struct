bf5f4c48fc817f5ed748d3bd81e4a3662bc7690b
jurrio <jurimik@yandex.ua>
2016-05-31 13:56:02 +0300

    added boolean isDeleted, removed SafeRemover

--- TestTask/src/com/eliftech/jurimik/servlet/DeleteCompanyServlet.java
+++ TestTask/src/com/eliftech/jurimik/servlet/DeleteCompanyServlet.java
@@ -10,9 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.eliftech.jurimik.constants.Messages;
 import com.eliftech.jurimik.constants.Parameters;
-import com.eliftech.jurimik.exception.UnknownCompanyException;
 import com.eliftech.jurimik.service.CompanyService;
-import com.eliftech.jurimik.util.SafeRemover;
 
 @WebServlet("/DeleteCompanyServlet")
 public class DeleteCompanyServlet extends HttpServlet {
@@ -26,19 +24,15 @@ public class DeleteCompanyServlet extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		long id = Long.parseLong(request.getParameter(Parameters.COMPANY_ID));
-		try {
-			SafeRemover.changeParentCompany(CompanyService.get(id));
-		} catch (UnknownCompanyException e) {
-			System.out.println("!!!");
-		}
-		/*boolean isDeleted = */
-		CompanyService.delete(id);
+		
+		boolean isDeleted = CompanyService.delete(id);
 			
-//		if (isDeleted) {
-		request.setAttribute(Parameters.MESSAGE, Messages.DELETE_SUCCESS);
-				
-//			}
-//			else request.setAttribute(Parameters.MESSAGE, Messages.DELETE_FAIL);
+		if (isDeleted) {
+			request.setAttribute(Parameters.MESSAGE, Messages.DELETE_SUCCESS);
+		} else {
+			request.setAttribute(Parameters.MESSAGE, Messages.DELETE_FAIL);
+		}
+		
 		request.setAttribute(Parameters.COMPANIES, CompanyService.getAll());
 		request.getRequestDispatcher("dashboard.jsp").forward(request, response);
 		

0eacdc8af950c1f580a74cb8613d0c1b241f44b9
Jurrio <jurimik@yandex.ua>
2016-06-05 23:19:05 +0300

    refactoring UpdateCompanyServlet
    - added check parameters for blank field
    - added check for chooze new main company (created class CompanyUtils).
    If chased new main company is a subsidiary or a subsidiary of a
    subsidiary value of parent id will not change.

--- TestTask/src/com/eliftech/jurimik/servlet/UpdateCompanyServlet.java
+++ TestTask/src/com/eliftech/jurimik/servlet/UpdateCompanyServlet.java
@@ -9,13 +9,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.eliftech.jurimik.builder.CompanyBuilder;
 import com.eliftech.jurimik.constants.Messages;
 import com.eliftech.jurimik.constants.Parameters;
 import com.eliftech.jurimik.exception.IllegalFormatEarningsException;
+import com.eliftech.jurimik.exception.IllegalIdException;
 import com.eliftech.jurimik.exception.UnknownCompanyException;
 import com.eliftech.jurimik.model.Company;
 import com.eliftech.jurimik.service.CompanyService;
+import com.eliftech.jurimik.util.CompanyUtils;
 import com.eliftech.jurimik.util.EarningsConverter;
+import com.eliftech.jurimik.util.ParamUtils;
 
 @WebServlet("/UpdateCompanyServlet")
 public class UpdateCompanyServlet extends HttpServlet {
@@ -23,17 +27,51 @@ public class UpdateCompanyServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setAttribute(Parameters.MESSAGE, Messages.VOID);
-		request.getRequestDispatcher("dashboard.jsp").forward(request, response);
+		long id = Long.parseLong(request.getParameter(Parameters.COMPANY_ID));
+		try {
+			Company company = CompanyService.get(id);
+			request.setAttribute(Parameters.COMPANY, company);
+			request.setAttribute(Parameters.COMPANY_ID, company.getId());
+			request.setAttribute(Parameters.COMPANIES, CompanyService.getAll());
+		} catch (NumberFormatException | UnknownCompanyException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		request.getRequestDispatcher("edit-company.jsp").forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		try {
 			Company company = CompanyService.get(Long.parseLong(request.getParameter(Parameters.COMPANY_ID)));
-			company.setName(request.getParameter(Parameters.NAME));
-			company.setParent(CompanyService.lazyGet(Long.parseLong(request.getParameter(Parameters.PARENT))));
-			company.setEarnings(EarningsConverter.get(request.getParameter(Parameters.EARNINGS)));
 			
-			boolean isUpdated = CompanyService.update(company);
+			String name = company.getName();
+			long earnings = company.getEarnings();
+			long parentId = (company.getParent() == null) ? 0 : company.getParent().getId();
+			
+			if (ParamUtils.isNotBlank(request.getParameter(Parameters.COMPANY))) {
+				name = request.getParameter(Parameters.NAME);
+			}
+			if (ParamUtils.isNotBlank(request.getParameter(Parameters.EARNINGS))) {
+				earnings = EarningsConverter.get(request.getParameter(Parameters.EARNINGS));
+			}
+			if (ParamUtils.isNotBlank(request.getParameter(Parameters.PARENT))) {
+				long newParentId = Long.parseLong(request.getParameter(Parameters.PARENT));
+				if (newParentId == company.getId()) {
+					throw new IllegalIdException("This id is itself!");
+				}
+				if (!CompanyUtils.isChildrenId(company.getId(), newParentId)) {
+					parentId = newParentId;
+				}
+			}
+			
+			Company updatedCompany = new CompanyBuilder(name, earnings).id(company.getId())
+					.parent(CompanyService.lazyGet(parentId)).build();
+						
+//			company.setName(request.getParameter(Parameters.NAME));
+//			company.setEarnings(EarningsConverter.get(request.getParameter(Parameters.EARNINGS)));
+			
+		
+			boolean isUpdated = CompanyService.update(updatedCompany);
 			
 			if (isUpdated) {
 				request.setAttribute(Parameters.MESSAGE, Messages.UPDATE_SUCCESS);
@@ -41,15 +79,20 @@ public class UpdateCompanyServlet extends HttpServlet {
 				request.setAttribute(Parameters.MESSAGE, Messages.UPDATE_FAIL);
 			}
 			
-			
+			request.setAttribute(Parameters.COMPANIES, CompanyService.getAll());
+						
 		} catch (UnknownCompanyException e) {
-			request.setAttribute(Parameters.MESSAGE, e.getMessage());
-		} catch (IllegalFormatEarningsException e) {
 			request.setAttribute(Parameters.MESSAGE, e.getMessage());
 		} catch (NumberFormatException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalIdException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalFormatEarningsException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} finally {
---
+++ TestTask/src/com/eliftech/jurimik/util/CompanyUtils.java
@@ -0,0 +1,26 @@
+package com.eliftech.jurimik.util;
+
+import java.util.List;
+
+import com.eliftech.jurimik.exception.UnknownCompanyException;
+import com.eliftech.jurimik.model.Company;
+import com.eliftech.jurimik.service.CompanyService;
+
+public class CompanyUtils {
+	
+	public static boolean isChildrenId(long id, long newParentId) throws UnknownCompanyException {
+		Company company = CompanyService.get(id);
+		List<Company> children = company.getChildren();
+		
+		if (!children.isEmpty()) {
+			for (Company child : children) {
+				if (child.getId() == newParentId) return true;
+				else isChildrenId(child.getId(), newParentId);
+			}
+		}
+		
+		return false;
+		
+	}
+
+}

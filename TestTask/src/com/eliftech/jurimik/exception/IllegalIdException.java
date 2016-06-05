29bb73664f8ffc45fb8d79f47f13eab5858d2ea9
Jurrio <jurimik@yandex.ua>
2016-06-05 22:25:55 +0300

    create class IllegalIdException

---
+++ TestTask/src/com/eliftech/jurimik/exception/IllegalIdException.java
@@ -0,0 +1,11 @@
+package com.eliftech.jurimik.exception;
+
+public class IllegalIdException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public IllegalIdException(String message) {
+		super(message);
+	}
+
+}

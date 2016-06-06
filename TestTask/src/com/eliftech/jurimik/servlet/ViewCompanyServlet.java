91d4b9d4cbbb880148ed5dfc847de70194d32e06
Jurrio <jurimik@yandex.ua>
2016-06-06 18:09:52 +0300

    implemented CompanyUtils.nestedChildren() in ViewCompanyServlet

--- TestTask/src/com/eliftech/jurimik/servlet/ViewCompanyServlet.java
+++ TestTask/src/com/eliftech/jurimik/servlet/ViewCompanyServlet.java
@@ -1,7 +1,9 @@
 package com.eliftech.jurimik.servlet;
 
 import java.io.IOException;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -14,12 +16,25 @@ import com.eliftech.jurimik.constants.Parameters;
 import com.eliftech.jurimik.exception.UnknownCompanyException;
 import com.eliftech.jurimik.model.Company;
 import com.eliftech.jurimik.service.CompanyService;
+import com.eliftech.jurimik.util.CompanyUtils;
 
 @WebServlet("/ViewCompanyServlet")
 public class ViewCompanyServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			Company company = new CompanyService().get(Long.parseLong(request.getParameter(Parameters.COMPANY_ID)));
+			Map<Company, Integer> nestedChildren = new LinkedHashMap<>();
+			nestedChildren = CompanyUtils.nestedChildren(nestedChildren, company.getId(), 0);
+			request.setAttribute(Parameters.COMPANY, company);
+			request.setAttribute(Parameters.MAP_COMPANIES, nestedChildren);
+		} catch (NumberFormatException | UnknownCompanyException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			request.getRequestDispatcher("view-company.jsp").forward(request, response);
+		}
 
 	}
 

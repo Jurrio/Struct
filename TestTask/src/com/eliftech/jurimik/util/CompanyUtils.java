375c6a014c7a6b7d48baec65e3c864adbaa61ee2
Jurrio <jurimik@yandex.ua>
2016-06-06 18:09:04 +0300

    inplemented method nestedChildren for find nested companies

--- TestTask/src/com/eliftech/jurimik/util/CompanyUtils.java
+++ TestTask/src/com/eliftech/jurimik/util/CompanyUtils.java
@@ -1,6 +1,8 @@
 package com.eliftech.jurimik.util;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.eliftech.jurimik.exception.UnknownCompanyException;
 import com.eliftech.jurimik.model.Company;
@@ -25,6 +27,19 @@ public class CompanyUtils {
 	
 	public static long parentId(Company company) {
 		return (company.getParent() == null) ? 0 : company.getParent().getId();
+	}
+	
+	public static Map<Company, Integer> nestedChildren(Map<Company, Integer> result, long id, int nested) throws UnknownCompanyException {
+		List<Company> children = new CompanyService().getChildren(id);
+		nested++;
+		for (Company child : children) {
+			result.put(child, nested);
+			System.out.println(child.getId() + " " + child.getName());
+			result.putAll(nestedChildren(result, child.getId(), nested));
+		}
+		nested--;
+		
+		return result;
 	}
 
 }
